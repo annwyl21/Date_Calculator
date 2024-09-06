bd94be424285609953c15d7c9a347fa221758099
Ellen Houghton <ellen.a.ash@gmail.com>
2024-09-06 20:02:41 +0100

    add a loop, append to log, grep the log

--- run_time_script.sh
+++ run_time_script.sh
@@ -2,17 +2,27 @@
 # set -x
 
 # A simple python script to determine the difference in seconds between now and 7.30pm
-# chmod +x run.sh
-# -x to debug
 
 echo "Starting Python Time Difference Calculator"
 
-python time_difference.py "19:30:00" 1> thelog.log 2> errorlog.log
+let count=0
 
-echo $?
-# return a status code; 0 for success and non-zero for unhappy code
+while [ $count -lt 10 ]
+do
+
+    python time_difference.py "19:30:00" >> thetimelog.log 2>> timeerrorlog.log
+
+    echo $?
+
+    let count=count+1
+
+    sleep 0.1
+
+done
 
 echo "Finished"
+
+grep "30:" thetimelog.log
 
 
 # Remember, It is going to a log file !!!

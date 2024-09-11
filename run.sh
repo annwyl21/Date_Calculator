de723d9b1286b12ba76be2040c4ee813fe171c31
Ellen Houghton <ellen.a.ash@gmail.com>
2024-09-11 15:53:34 +0100

    using arguments

--- run.sh
+++ run.sh
@@ -10,7 +10,10 @@
 
 echo "Starting Python Date Difference Calculator"
 
-python date_difference.py 2015-11-27 2024-09-06 1> thelog.log 2> errorlog.log
+# python date_difference.py 2015-11-27 2024-09-06 1> thelog.log 2> errorlog.log
+python date_difference.py $1 $2 1> thelog.log 2> errorlog.log
+# bash ./run.sh "2015-11-27" "2024-09-06"
+# Remember stdout is a log, the status code goes to the terminal!
 
 echo $?
 # return a status code; 0 for success and non-zero for unhappy code

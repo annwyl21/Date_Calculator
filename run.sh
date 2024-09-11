3e24037d1a0c995fb7b9a3b943970fbe68026b6a
Ellen Houghton <ellen.a.ash@gmail.com>
2024-09-11 16:06:07 +0100

    add conditional to check argument number is correct

--- run.sh
+++ run.sh
@@ -10,8 +10,19 @@
 
 echo "Starting Python Date Difference Calculator"
 
+echo $#
+
+if [ $# -lt 2 ]
+then 
+
+    echo "add arguments"
+
+    exit 1
+
+fi
+
 # python date_difference.py 2015-11-27 2024-09-06 1> thelog.log 2> errorlog.log
-python date_difference.py $1 $2 1> thelog.log 2> errorlog.log
+    python date_difference.py $1 $2 1> thelog.log 2> errorlog.log
 # bash ./run.sh "2015-11-27" "2024-09-06"
 # Remember stdout is a log, the status code goes to the terminal!
 
